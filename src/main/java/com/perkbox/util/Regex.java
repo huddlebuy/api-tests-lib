3cf249a672d4f72de6964f6737e253f82cb5d647
Samuel Chinenyeze <samuelc@perkbox.co.uk>
2018-06-18 15:55:35 +0100

    Add a Regex helper class

---
+++ src/main/java/com/perkbox/util/Regex.java
@@ -0,0 +1,8 @@
+package com.perkbox.util;
+
+public class Regex {
+
+    public static final String ANY = ".*";
+    public static final String UUID = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}";
+
+}
